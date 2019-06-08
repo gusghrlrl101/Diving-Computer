f89fe688aa41a9f0aff7290372d5aa66198e9342
gusghrlrl101 <gusghrlrl123@naver.com>
2019-06-08 20:35:53 +0900

    sensor completed

--- CCS/hyunho/main.c
+++ CCS/hyunho/main.c
@@ -3,10 +3,9 @@
 #include "lcd.h"
 #include "timer.h"
 #include "switch.h"
-
 #define RD_PROM 0xA0
 #define RRESET   0x1E
-#define CONV_P  0x40
+#define CONV_P  0x44
 #define CONV_T  0x50
 #define READ    0x00
 
@@ -28,50 +27,11 @@ unsigned int coefficient[8];       // Coefficients;
 unsigned long result_temp;
 unsigned long result_pres;
 
-double time__;
-
 void read_prom();
 void send_cmd(unsigned char data);
-void recv_data();
+void recv_data2();
+void recv_data3();
 void calc_data();
-
-void read_prom()
-{
-    unsigned char j;
-    for (j = 0; j <= 7; j++)
-    {
-        send_cmd(0xA0 + (j * 2));
-        recv_data();
-        coefficient[j] = (RxBuffer[0] << 8) | RxBuffer[1];
-    }
-}
-
-void send_cmd(unsigned char data)
-{
-    TXData = data;
-    //delay_long();
-    TXByteCtr = 1;                          // Load TX byte counter
-
-    while (UCB1CTLW0 & UCTXSTP)
-        ;             // Ensure stop condition got sent
-    UCB1CTLW0 |= (UCTR | UCTXSTT);             // I2C TX, start condition
-
-    __enable_interrupt();
-}
-
-void recv_data()
-{
-    PRxData = (unsigned char *) RxBuffer;    // Start of RX buffer
-    RXByteCtr = 0;                          // Clear RX byte count
-
-    while (UCB1CTLW0 & UCTXSTP)
-        ;             // Ensure stop condition got sent
-    UCB1CTLW0 |= (~UCTR & UCTXSTT);                    // I2C start condition
-    while (UCB1CTLW0 & UCTXSTT)
-        ;              // Start condition sent?
-    __delay_cycles(400);
-    UCB1CTLW0 |= UCTXSTP;                    // I2C stop condition
-}
 
 void calc_data()
 {
@@ -132,6 +92,61 @@ void calc_data()
     pressure_reported = pressure_reported / 10.0f;
     __no_operation();
 }
+void read_prom()
+{
+    unsigned char j;
+    for (j = 0; j <= 7; j++)
+    {
+        send_cmd(0xA0 + (j * 2));
+        __delay_cycles(100);
+        recv_data2();
+        __delay_cycles(3000);
+        coefficient[j] = (RxBuffer[0] << 8) | RxBuffer[1];
+    }
+}
+
+void send_cmd(unsigned char data)
+{
+    TXData = data;
+    TXByteCtr = 1;                          // Load TX byte counter
+
+    while (UCB1CTLW0 & UCTXSTP)
+    {
+
+    }
+    UCB1CTLW0 |= UCTR + UCTXSTT;             // I2C TX, start condition
+
+    __enable_interrupt();
+}
+
+void recv_data2()
+{
+    UCB1TBCNT = 2;  // number of bytes to be received
+
+    PRxData = (unsigned char *) RxBuffer;    // Start of RX buffer
+    RXByteCtr = 1;                          // Clear RX byte count
+
+    while (UCB1CTLW0 & UCTXSTP){
+
+    }
+    UCB1CTLW0 &= ~UCTR;
+    UCB1CTLW0 |= UCTXSTT;
+}
+
+void recv_data3()
+{
+    UCB1TBCNT = 3;  // number of bytes to be received
+
+    PRxData = (unsigned char *) RxBuffer;    // Start of RX buffer
+    RXByteCtr = 1;                          // Clear RX byte count
+
+    while (UCB1CTLW0 & UCTXSTP)
+    {
+
+    }
+    UCB1CTLW0 &= ~UCTR;
+    UCB1CTLW0 |= UCTXSTT;
+}
 
 void main(void)
 {
@@ -139,10 +154,6 @@ void main(void)
     switch_init();
     lcd_init();
     lcd_first();
-
-    // P3.4 -> SMCLK
-    P3DIR |= BIT4;
-    P3SEL1 |= BIT4;
 
     // P5.0 -> SDA (UCB1CTL1)
     P5SEL1 &= ~BIT0;
@@ -153,36 +164,52 @@ void main(void)
     P5SEL0 |= BIT1;
 
     UCB1CTLW0 = (UCSWRST | UCSSEL_2 | UCMST | UCMODE_3 | UCSYNC);
-    UCB1BRW = 12;   // bit clock prescaler
+    UCB1BRW = 11;   // bit clock prescaler
     UCB1I2CSA = 0x77;   // slave address
     UCB1CTLW0 &= ~UCSWRST;
-
-    UCB1IE |= (UCTXIE | UCRXIE);
+    UCB1CTLW1 |= UCASTP_2;
+    UCB1IE |= (UCTXIE0 | UCRXIE0);
 
     while (1)
+    {
+
         send_cmd(RRESET);
-    __delay_cycles(4000);
-    send_cmd(READ);
-    recv_data();
-    result_temp = ((unsigned long) RxBuffer[0] << 16)
-            + ((unsigned long) RxBuffer[1] << 8) + RxBuffer[2];
+        __delay_cycles(4000);
+        read_prom();
+        __delay_cycles(3000);
 
-    send_cmd(CONV_P);
-    __delay_cycles(4000);
-    send_cmd(READ);
-    recv_data();
-    result_pres = ((unsigned long) RxBuffer[0] << 16)
-            + ((unsigned long) RxBuffer[1] << 8) + RxBuffer[2];
+        send_cmd(CONV_T);
+        __delay_cycles(3000);
+        send_cmd(READ);
+        __delay_cycles(100);
+        recv_data3();
+        __delay_cycles(3000);
+        result_temp = ((unsigned long) RxBuffer[0] << 16)
+                + ((unsigned long) RxBuffer[1] << 8) + RxBuffer[2];
 
-    calc_data();
+        send_cmd(CONV_P);
+        __delay_cycles(3000);
+        send_cmd(READ);
+        __delay_cycles(100);
+        recv_data3();
+        __delay_cycles(3000);
+        result_pres = ((unsigned long) RxBuffer[0] << 16)
+                + ((unsigned long) RxBuffer[1] << 8) + RxBuffer[2];
 
-    // make_sample_data();
+        calc_data();
+    }
 }
 
-#pragma vector=USCI_B1_VECTOR
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = EUSCI_B1_VECTOR
 __interrupt void USCI_B1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(EUSCI_B1_VECTOR))) USCI_B1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
 {
-    switch (__even_in_range(UCB1IV, 12))
+    switch (__even_in_range(UCB1IV, USCI_I2C_UCBIT9IFG))
     {
     case 0:
         break;                           // Vector  0: No interrupts
@@ -190,30 +217,19 @@ __interrupt void USCI_B1_ISR(void)
         break;                           // Vector  2: ALIFG
     case 4:
         break;                           // Vector  4: NACKIFG
-    case 6:                                  // Vector  6: STTIFG
-        UCB1IFG &= ~UCSTTIFG;
-        break;
-    case 8:                                  // Vector  8: STPIFG
-        UCB1IFG &= ~UCSTPIFG;
-        if (RXByteCtr)                          // Check RX byte counter
-            __bic_SR_register_on_exit(LPM0_bits + GIE);
-        break;
-    case 10:                                  // Vector 10: RXIFG
+    case USCI_I2C_UCRXIFG0:                                  // Vector 10: RXIFG
         *PRxData++ = UCB1RXBUF;                 // Get RX'd byte into buffer
         RXByteCtr++;
+
         break;
-    case 12:                                  // Vector 12: TXIFG
+    case USCI_I2C_UCTXIFG0:                                  // Vector 12: TXIFG
         if (TXByteCtr)                          // Check TX byte counter
         {
             UCB1TXBUF = TXData;                   // Load TX buffer
             TXByteCtr--;                          // Decrement TX byte counter
         }
         else
-        {
             UCB1CTLW0 |= UCTXSTP;                  // I2C stop condition
-            UCB1IFG &= ~UCTXIFG;                  // Clear USCI_B0 TX int flag
-            __bic_SR_register_on_exit(LPM0_bits + GIE); // Exit LPM0
-        }
         break;
     default:
         break;
